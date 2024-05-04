d7c0e0f09ccb390d4f92f90585b79be954fa1dec
IWantToUseGitHub <110896712+IWantToUseGitHub@users.noreply.github.com>
2024-05-04 17:44:50 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    var activeTab = tabs[0];
+    console.log(activeTab.url);
+});
+
+var toggle = document.getElementById("toggle");
+
+toggle.addEventListener("change", function() {
+    if (toggle.checked) {
+        console.log("Detector enabled, checking every links clicked.");
+    } else {
+        console.log("Detector disabled, links are not checked.");
+    }
+});
+
+// Reference:
+// https://docs.virustotal.com/
