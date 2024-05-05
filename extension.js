9c0fc41074379d92b91c5b65ca35158f151a99de
IWantToUseGitHub <110896712+IWantToUseGitHub@users.noreply.github.com>
2024-05-05 16:46:39 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,9 +1,17 @@
-browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    var activeTab = tabs[0];
-    console.log(activeTab.url);
-});
-
+// Gets element from HTML to tell if checkbox is enabled or disabled.
 var toggle = document.getElementById("toggle");
+
+// VirusTotal API
+const apiKey = "";
+const apiURL = "";
+let scanURL = "";
+
+// Checks and gets active tab URL.
+browser.tabs.query({ active: true, currentWindow: true }, function(tabs){
+    var activeTab = tabs[0];
+    scanURL = activeTab.url;
+    console.log("URL of Active Tab: ", activeTab.url);
+});
 
 toggle.addEventListener("change", function() {
     if (toggle.checked) {
